7ab24848d8efa67f618d34195c27b4c296ac9649
sev28 <charlisev@hotmail.fr>
2023-11-11 09:08:46 +0100

    creation Entities Migration CDN bootstrap

---
+++ src/Entity/Category.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $wording = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getWording(): ?string
+    {
+        return $this->wording;
+    }
+
+    public function setWording(string $wording): static
+    {
+        $this->wording = $wording;
+
+        return $this;
+    }
+}
